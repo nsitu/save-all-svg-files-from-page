5a39c5291e36bcdc8391fee757f057956d4975f9
Harold Sikkema <harold@nsitu.ca>
2022-05-10 15:11:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,22 @@
 /* it will attempt to locate a name for the svg from the innerHTML of nearby elements. */
 
 document.querySelectorAll('svg').forEach( svg => {   
-  var svgData = svg.outerHTML;
-  var iconName = 'icon.svg'
+  let svgData = svg.outerHTML;
+  let iconName = 'icon.svg'
   if (svg.nextSibling != null){
    iconName = svg.nextSibling.innerHTML
   }
   else if (svg.parentElement.nextSibling != null){
    iconName = svg.parentElement.nextSibling.innerHTML
   }
-  iconName = iconName.replace(/(<([^>]+)>)/gi, "") /*strip tags */
-  iconName = iconName.replace(' ', '_') /* underscore spaces*/
-  var svgBlob = new Blob([svgData], {type:"image/svg+xml;charset=utf-8"});
-  var svgUrl = URL.createObjectURL(svgBlob);
-  var downloadLink = document.createElement("a");
+  /* strip all html tags from the file name.
+  See also: https://css-tricks.com/snippets/javascript/strip-html-tags-in-javascript/ */
+  iconName = iconName.replace(/(<([^>]+)>)/gi, "") 
+  /* use underscores instead of spaces*/
+  iconName = iconName.replace(' ', '_') 
+  let svgBlob = new Blob([svgData], {type:"image/svg+xml;charset=utf-8"});
+  let svgUrl = URL.createObjectURL(svgBlob);
+  let downloadLink = document.createElement("a");
   downloadLink.href = svgUrl;
   downloadLink.download = iconName +".svg";
   document.body.appendChild(downloadLink);
